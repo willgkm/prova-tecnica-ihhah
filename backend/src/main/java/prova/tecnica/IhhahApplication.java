7f581e6edf077ff5c102a91f8a232e911fa94a6e
williangk <wkuviatkovski@gmail.com>
2024-10-16 00:54:32 -0300

    Criando projeto

---
+++ backend/src/main/java/prova/tecnica/IhhahApplication.java
@@ -0,0 +1,13 @@
+package prova.tecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IhhahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IhhahApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/prova/tecnica/IhhahApplicationTests.java
@@ -0,0 +1,13 @@
+package prova.tecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IhhahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
