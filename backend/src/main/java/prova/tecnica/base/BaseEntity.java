19f93e8e1fae066a25cb6472f9288bacbba02e43
williangk <wkuviatkovski@gmail.com>
2024-10-16 13:47:34 -0300

    criando BaseEntity

---
+++ backend/src/main/java/prova/tecnica/base/BaseEntity.java
@@ -0,0 +1,20 @@
+package prova.tecnica.base;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
