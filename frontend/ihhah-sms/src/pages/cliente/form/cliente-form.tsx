ac5987f5b9f23a82119cb2e2147ef9ff1581b862
williangk <wkuviatkovski@gmail.com>
2024-10-17 20:08:36 -0300

    melhorando controle de estado, e validacao do formulario

--- frontend/ihhah-sms/src/pages/cliente/form/cliente-form.tsx
+++ frontend/ihhah-sms/src/pages/cliente/form/cliente-form.tsx
@@ -1,99 +1,111 @@
 import { useEffect, useState } from 'react';
-import {
-  Button,
-  Form,
-  Input,
-  Select,
-} from 'antd';
-
-
-import { ClienteType } from '../../../models/ClienteType';
-import clienteService from '../../../services/clienteService';
+import { Button, Form, Input, Select } from 'antd';
 import { PlanoType } from '../../../models/PlanoType';
 import planoService from '../../../services/planoService';
+import { useParams } from 'react-router-dom';
 
-
+const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const digitsOnlyPattern = /^\d+$/;
+const phonePattern = /^\d{2} \d{5}-\d{4}$/; 
 
 export default function ClienteFormPage() {
-  const [planos, setPlanos] = useState<PlanoType[]>([])
-  const [formValues, setFormValues] = useState<ClienteType>({
-    nome: '',
-    email: '',
-    telefone: '',
-    cpf: '',
-    cnpj: '',
-    nomeEmpresa: '',
-    saldo: 10,
-  });
-  
-  useEffect(() => {    
+  const [form] = Form.useForm();
+  const [planos, setPlanos] = useState<PlanoType[]>([]);
+  const { id } = useParams(); 
+
+  useEffect(() => {
     fetchPlanos();
   }, []);
-  
+
   async function fetchPlanos() {
     try {
       const data = await planoService.getAll();
       setPlanos(data);
     } catch (error) {
-      console.error('Erro ao buscar clientes:', error);
+      console.error('Erro ao buscar planos:', error);
     }
-  };
-  
-  function submit(event: any) {
-    event.preventDefault();
-    save(); 
-  };
-
-  function save (){
-    clienteService.save(formValues)
   }
-  
-  function handleChange(changedValues: any) {
-    setFormValues((prevValues) => ({
-      ...prevValues,
-      ...changedValues,
-    }));
+
+  const submit = async (values: any) => {
+    const formattedValues = {
+      ...values,
+      id: id || null , 
+      plano: { id: values.plano.nome },
+    };
+    console.log('Form values:', formattedValues);
   };
 
-  function handlePlanoChange(value: number) {
-    const selectedPlano = planos.find(plano => plano.id === value);
-    setFormValues(prev => ({
-      ...prev,
-      plano: selectedPlano || undefined,
-    }));
-  };
+  const handlePlanoChange = (value: number) => {
 
+    form.setFieldsValue({ plano: {id: value } }); 
+
+  };
 
   return (
     <Form
+      form={form}
       labelCol={{ span: 6 }}
       wrapperCol={{ span: 14 }}
       layout="horizontal"
       size={"middle"}
-      onValuesChange={handleChange}
       onFinish={submit}
       style={{ maxWidth: 600 }}
     >
-
       <Form.Item label="Nome" name="nome" rules={[{ required: true, message: 'Por favor insira o nome!' }]}>
         <Input />
       </Form.Item>
-      <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Por favor insira o email!' }]}>
+      <Form.Item 
+        label="Email" 
+        name="email" 
+        rules={[
+          { required: true, message: 'Por favor insira o email!' },
+          { pattern: emailPattern, message: 'Formato de email inválido!' }
+        ]}
+      >
+        <Input placeholder="email@dominio.com" />
+      </Form.Item>
+      <Form.Item 
+        label="Telefone" 
+        name="telefone" 
+        rules={[
+          { required: true, message: 'Por favor insira o telefone!' },
+          { pattern: phonePattern, message: 'Formato inválido! Use (99) 99999-9999' }
+        ]}
+      >
+        <Input placeholder="(99) 99999-9999" />
+      </Form.Item>
+      <Form.Item 
+        label="CPF" 
+        name="cpf" 
+        rules={[
+          { required: true, message: 'Por favor insira o CPF!' },
+          { pattern: digitsOnlyPattern, message: 'O CPF deve conter apenas dígitos!' }
+        ]}
+      >
         <Input />
       </Form.Item>
-      <Form.Item label="Telefone" name="telefone" rules={[{ required: true, message: 'Por favor insira o telefone!' }]}>
+      <Form.Item 
+        label="CNPJ" 
+        name="cnpj" 
+        rules={[
+          { required: true, message: 'Por favor insira o CNPJ!' },
+          { pattern: digitsOnlyPattern, message: 'O CNPJ deve conter apenas dígitos!' }
+        ]}
+      >
         <Input />
       </Form.Item>
-      <Form.Item label="CPF" name="cpf" rules={[{ required: true, message: 'Por favor insira o CPF!' }]}>
+      <Form.Item 
+        label="Nome da Empresa" 
+        name="nomeEmpresa" 
+        rules={[{ required: true, message: 'Por favor insira o nome da empresa!' }]}
+      >
         <Input />
       </Form.Item>
-      <Form.Item label="CNPJ" name="cnpj" rules={[{ required: true, message: 'Por favor insira o CNPJ!' }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Nome da Empresa" name="nomeEmpresa" rules={[{ required: true, message: 'Por favor insira o nome da empresa!' }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Plano" name={['plano', 'nome']} rules={[{ required: true, message: 'Por favor selecione um plano!' }]}>
+      <Form.Item 
+        label="Plano" 
+        name={['plano', 'nome']}
+        rules={[{ required: true, message: 'Por favor selecione um plano!' }]}
+      >
         <Select onChange={handlePlanoChange}>
           {planos.map(plano => (
             <Select.Option key={plano.id} value={plano.id}>{plano.nome}</Select.Option>
@@ -105,4 +117,4 @@ export default function ClienteFormPage() {
       </Form.Item>
     </Form>
   );
-};
+}
