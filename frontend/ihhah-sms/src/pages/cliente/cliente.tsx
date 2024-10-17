37273a89fae0502227834e9dd68d1a802095b864
williangk <wkuviatkovski@gmail.com>
2024-10-17 17:24:56 -0300

    criando actions da tabela de cliente

--- frontend/ihhah-sms/src/pages/cliente/cliente.tsx
+++ frontend/ihhah-sms/src/pages/cliente/cliente.tsx
@@ -1,25 +1,38 @@
-import { TableProps, Tag, Tooltip } from "antd";
-import ClienteService  from '../../services/clienteService';
+import { Button, Space, TableProps, Tag, Tooltip } from "antd";
+import ClienteService from '../../services/clienteService';
 import { ClienteType } from "../../models/ClienteType";
 import IhhahTable from "../../components/ihhahTable";
 import { useEffect, useState } from "react";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import clienteService from "../../services/clienteService";
 
 export default function ClientePage() {
 
 	const [clienteData, setClienteData] = useState<ClienteType[]>([]);
 
-	useEffect(() => {
-		const fetchClientes = async () => {
-		  try {
+	useEffect(() => {		
+		fetchClientes();
+	}, []);
+
+	async function fetchClientes() {
+		try {
 			const data = await ClienteService.getAll();
 			setClienteData(data);
-		  } catch (error) {
+		} catch (error) {
 			console.error('Erro ao buscar clientes:', error);
-		  }
-		};
+		}
+	};
 	
-		fetchClientes();
-	  }, []);
+	function edit(id: number) {
+		console.log("edit");
+		
+	}
+
+	async function remove(id: number) {
+		await clienteService.delete(id);
+		await fetchClientes();
+			
+	}
 
 	const columns: TableProps<ClienteType>['columns'] = [
 		{
@@ -41,17 +54,17 @@ export default function ClientePage() {
 			title: 'Nome da Empresa',
 			dataIndex: 'nomeEmpresa',
 			key: 'nomeEmpresa',
-			render: (_, cliente ) => (  
-			<Tooltip title={`CNPJ: ${cliente.cnpj}, CPF: ${cliente.cpf}`}>
-				<span>{cliente.nomeEmpresa}</span>
-			</Tooltip>), 
+			render: (_, cliente) => (
+				<Tooltip title={`CNPJ: ${cliente.cnpj}, CPF: ${cliente.cpf}`}>
+					<span>{cliente.nomeEmpresa}</span>
+				</Tooltip>),
 
 		},
 		{
 			title: 'Plano',
 			dataIndex: 'plano.nome',
 			key: 'plano',
-			render: (_, cliente ) => <Tag>{cliente.plano!.nome}</Tag>, 
+			render: (_, cliente) => <Tag>{cliente.plano!.nome}</Tag>,
 		},
 		{
 			title: 'Saldo',
@@ -68,31 +81,43 @@ export default function ClientePage() {
 			title: 'Limite',
 			dataIndex: 'limite',
 			key: 'limite',
-			render: (_, cliente ) => cliente.plano!.id == 1 ?
-			(<span>Cliente Pre-pago</span>) : 
-			(
-			<>
-				{ new Intl.NumberFormat('pt-BR', {
-					style: 'currency',
-					currency: 'BRL',
-				}).format(cliente.plano!.limiteConsumo)}
-			</>) , 
+			render: (_, cliente) => cliente.plano!.id == 1 ?
+				(<span>Cliente Pre-pago</span>) :
+				(
+					<>
+						{new Intl.NumberFormat('pt-BR', {
+							style: 'currency',
+							currency: 'BRL',
+						}).format(cliente.plano!.limiteConsumo)}
+					</>),
 
 		},
+		{
+			title: 'Ações',
+			key: 'action',
+			render: (_, cliente) => (
+				<Space size="middle">
+					<Tooltip title="edit" >
+        		<Button type="primary" shape="circle" icon={<EditOutlined />} onClick={() => edit(cliente.id!)}/>
+					</Tooltip>
+					<Tooltip title="delete" >
+						<Button type="primary" danger shape="circle" icon={<DeleteOutlined/>} onClick={() => remove(cliente.id!)}/>
+					</Tooltip>
+					
+				</Space>
+			),
+		},
 	];
-	
 
 	return (
 		<>
 			<IhhahTable
-				label="cliente" 
-				data={clienteData} 
-				columns={columns} 
-				formLink="/cliente/novo" 
+				label="cliente"
+				data={clienteData}
+				columns={columns}
+				formLink="/cliente/novo"
 			/>
 		</>
-
-
 	)
 
 
