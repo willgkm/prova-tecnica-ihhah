27b9d3d12aa377607a3615c605879ff2d703261e
williangk <wkuviatkovski@gmail.com>
2024-10-18 15:52:59 -0300

    arrumando footer e removendo alguma coisas do app.tsx

--- frontend/ihhah-sms/src/App.tsx
+++ frontend/ihhah-sms/src/App.tsx
@@ -1,35 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { LaptopOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
-import HomePage from './pages/home/homepage';
+import { Layout, Menu, theme } from 'antd';
 import ClientePage from './pages/cliente/cliente';
-import PlanoPage from './pages/plano/plano';
-import MensagemPage from './pages/mensagem/mensagem';
+import ClienteFormPage from './pages/cliente/form/cliente-form';
 
 const { Header, Content, Footer, Sider } = Layout;
 
 const sidebarItems: MenuProps['items'] = [
   {
     key: '1',
-    icon: <UserOutlined />,
-    label: <Link to="/">Home</Link>,
-  },
-  {
-    key: '2',
     icon: <LaptopOutlined />,
     label: <Link to="/cliente">Cliente</Link>,
-  },
-  {
-    key: '3',
-    icon: <NotificationOutlined />,
-    label: <Link to="/plano">Plano</Link>,
-  },
-  {
-    key: '4',
-    icon: <NotificationOutlined />,
-    label: <Link to="/mensagem">Mensagem</Link>,
   },
 ];
 
@@ -41,11 +24,11 @@ const App: React.FC = () => {
 
   return (
     <Router>
-      <Layout>
-        <Header style={{ display: 'flex', alignItems: 'center' }}>
+      <Layout style={{ minHeight: '100vh' }}>
+      <Header style={{ display: 'flex', alignItems: 'center' }}>
             <div style={{ color: '#fff', fontSize: '20px' }}>ihhah-sms</div>
         </Header>
-        <Content style={{ padding: '0 48px' }}>
+        <Content style={{ padding: '0 0'}}>
           <Layout
             style={{ padding: '24px 0', background: colorBgContainer, borderRadius: borderRadiusLG }}
           >
@@ -57,18 +40,18 @@ const App: React.FC = () => {
                 items={sidebarItems}
               />
             </Sider>
-            <Content style={{ padding: '0 24px', minHeight: 280 }}>
+            <Content style={{ padding: '0 2rem', minHeight: 'calc(100vh - 200px)' }}>
               <Routes>
-                <Route path="/" element={<HomePage />} />
+                <Route path="/" element={<ClientePage />} />
                 <Route path="/cliente" element={<ClientePage />} />
-                <Route path="/plano" element={<PlanoPage />} />
-                <Route path="/mensagem" element={<MensagemPage />} />
+                <Route path="/cliente/novo" element={<ClienteFormPage />} />
+                <Route path="/cliente/:id" element={<ClienteFormPage />} />
               </Routes>
             </Content>
           </Layout>
         </Content>
         <Footer style={{ textAlign: 'center' }}>
-          Apenas uma prova tecnica, utilizando componetes do AntDesing !
+          Utilizando componetes do AntDesing !
         </Footer>
       </Layout>
     </Router>
