e68baf9c7d0131b2189c325a8588403121fde6c6
williangk <wkuviatkovski@gmail.com>
2024-10-18 16:47:35 -0300

    removendo @service

--- backend/src/main/java/prova/tecnica/base/BaseService.java
+++ backend/src/main/java/prova/tecnica/base/BaseService.java
@@ -5,14 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Getter
 @Primary
-@Service
 public class BaseService<ENTITY extends BaseEntity,
         REPOSITORY extends JpaRepository<ENTITY, Long>> {
 
